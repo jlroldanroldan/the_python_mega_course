8b60eb5f3ebe049cfe89be3ad2e0f1263523314c
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-02 16:05:54 -0400

    Notes up to lecture 50

---
+++ lecture46_for_loops.py
@@ -0,0 +1,9 @@
+monday_temperatures = [9.1, 8.8, 7.6]
+
+
+for temperature in monday_temperatures:
+    print(round(temperature))
+
+
+for letter in "hello":
+    print(letter.title())
---
+++ lecture49_looping_through_a_dictionary.py
@@ -0,0 +1,13 @@
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+for grades in student_grades.items():
+    print(grades)
+    
+
+
+for grades in student_grades.values():
+    print(grades)
+
+
+for grades in student_grades.keys():
+    print(grades)
