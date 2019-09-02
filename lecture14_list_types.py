f2ebe090dfbf20258d8301de62ec2df173a1482c
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-02 11:03:13 -0400

    Notes up to lectures 19

---
+++ lecture14_list_types.py
@@ -0,0 +1,6 @@
+student_grades = [9.1, 8.8, 7.5]
+print(student_grades)
+
+print(list(range(1,10)))
+
+print(list(range(1, 10, 2)))
---
+++ lecture16_type_attributes.py
@@ -0,0 +1,11 @@
+student_grades = [9.1, 8.8, 7.5]
+print(student_grades)
+
+print(list(range(1,10)))
+
+print(list(range(1, 10, 2)))
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
