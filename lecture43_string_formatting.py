f04430149e3e848f5530524771d7daba705b3aa2
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-02 15:46:04 -0400

    Notes up to lecture 46

---
+++ lecture43_string_formatting.py
@@ -0,0 +1,7 @@
+
+user_input = input("Enter your name: ")
+message1 = "Hello %s!" % user_input
+message2 = "Hello {}!".format(user_input)
+print(message1)
+print(message2)
+
---
+++ lecture44_string_formatting_2.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your name:")
+surname = input("Enter your name: ")
+
+
+message1 = "Hello %s %s!" % (name, surname)
+message2 = "Hello {} {}!".format(name, surname)
+
+print(message1)
+print(message2)
+
