0f720d18a2be474ac6f64ad64c356ae48819b433
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-04 14:45:21 -0400

    Notes up to lecture 84.  First 12 sections finished.

--- lecture82_third_party_modules.py
+++ lecture82_third_party_modules.py
@@ -1,13 +1,12 @@
 import time
 import os
-
+import pandas
 
 while True:
-    if os.path.exists("Files/vegetables.txt"):
-        with open("Files/vegetables.txt") as file:
-            time.sleep(2)
-            print(file.read())
+    if os.path.exists("Files/temps-today.csv"):
+        with open("Files/temps-today.csv") as file:
+            data = pandas.read_csv("Files/temps-today.csv")
+            print(data.mean())
     else:
         print("File does not exitst.")
     time.sleep(10)
-
