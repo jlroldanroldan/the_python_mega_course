4be24f4af2fcc6ca45248c83abc52451d0e20fb7
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-09 13:52:45 -0400

    Notes up to lecture 147

--- app3_website_blocker/website_blocker.py
+++ app3_website_blocker/website_blocker.py
@@ -1,15 +1,23 @@
 import time
 from datetime import datetime as dt
 
-hosts_path = "/etc/hosts"
+hosts_temp = "hosts"
+hosts_path = "\etc\hosts"
 redirects = "127.0.0.1"
 website_lists = ["www.facebook.com", "facebok.com", "www.youtube.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working hours...")
+        with open(hosts_temp, "r+") as file:
+            content = file.read()
+            print(content)
+            for website in website_lists:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirects + " " + website + "\n")
     else:
         print("Fun hours...")
 
-    print(1)
     time.sleep(5)
